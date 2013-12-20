8abda2263c1aa925cde2cf565a74235c6a88fcc9
kkelleey <karenkelley11@gmail.com>
2013-12-19 21:04:03 -0500

    changed migrations

--- db/migrate/20131130015154_change_column_type.rb
+++ db/migrate/20131130015154_change_column_type.rb
@@ -1,5 +1,5 @@
 class ChangeColumnType < ActiveRecord::Migration
   def change
-  	change_column :items, 'integer USING CAST(category AS integer)'
+  	change_column :items, :category, 'integer USING CAST(category AS integer)'
   end
 end
