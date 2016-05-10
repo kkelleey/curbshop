e5814fbf2d82ed5a52b9a8a134302be7f523956e
Karen Kelley <karenkelley11@gmail.com>
2016-05-10 15:14:03 -0500

    Adding .fetch to get env variables
    This is so that an error will be raised if the variable is not set
    
    http://eng.joingrouper.com/blog/2014/09/02/configuring-rails-environments/

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,4 +1,4 @@
 Instagram.configure do |config|
-  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
-  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
+  config.client_id = ENV.fetch('INSTAGRAM_CLIENT_ID')
+  config.client_secret = ENV.fetch('INSTAGRAM_CLIENT_SECRET')
 end
