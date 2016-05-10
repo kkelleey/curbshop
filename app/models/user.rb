19b386ddf05fcdf755be17d02d068ff9c23ed131
Karen Kelley <karenkelley11@gmail.com>
2016-05-10 14:55:17 -0500

    Moving Instagram config to an initializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,6 @@ class User < ActiveRecord::Base
   def instagram_images
     return nil if instagram_username.nil?
 
-    Instagram.configure do |config|
-      config.client_id = '8e29ac2743964cb4844c022fc7a2ee08'
-      config.client_secret = 'ee7b3c9f5aeb47429391196cd8f06c70'
-    end
     instagram_id = Instagram.user_search(instagram_username).first.id
     urls = []
     Instagram.user_recent_media(instagram_id).each do |image|
