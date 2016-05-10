49229aa11ab126a4ec7bffe199f5d2e25057aaea
Karen Kelley <karenkelley11@gmail.com>
2016-05-10 16:06:10 -0500

    formatting bid model

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,17 +1,12 @@
 class Bid < ActiveRecord::Base
-	belongs_to :item
-	belongs_to :user
+  belongs_to :item
+  belongs_to :user
 
-	validate :bid_greater_than_current_price, on: :create
+  validate :bid_greater_than_current_price, on: :create
 
-  #Nice, this is a good use of validations	
-	def bid_greater_than_current_price
-		if bid < item.current_price
-			errors.add(:bid, "can't be lower than current price")
-		end
-	end
-
+  def bid_greater_than_current_price
+    if bid < item.current_price
+      errors.add(:bid, "can't be lower than current price")
+    end
+  end
 end
-
-
-
