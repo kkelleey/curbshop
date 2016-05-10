5f564b65ca293ac0ff695ec8ee06d0e5a064aef3
Karen Kelley <karenkelley11@gmail.com>
2016-05-10 16:09:14 -0500

    Cleaning up catebory.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,5 @@
 class Category < ActiveRecord::Base
-
-  #Nice advanced use of ActiveRecord!
-  has_many :subcategories, :class_name => "Category", :foreign_key => "parent_id", :dependent => :destroy
-  belongs_to :parent_category, :class_name => "Category"
-
+  has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id',
+                           dependent: :destroy
+  belongs_to :parent_category, class_name: 'Category'
 end
