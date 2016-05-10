b33935e8dfc69ea42def2bf143e0b3bcab224999
Karen Kelley <karenkelley11@gmail.com>
2016-05-10 15:34:25 -0500

    fixing formatting in item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,38 +1,32 @@
 class Item < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :user
+  has_many :item_images, dependent: :destroy
+  accepts_nested_attributes_for :item_images
+  has_many :bids
 
-  #Very nice use of custom convenience methods (e.g. `highest_bid`)
-  #Remember that Ruby employs an explicit return for methods, so you only need to specify them if your control flow absolutely requires it.
-  #Also note usage of the ternary operator ( `condition ? true case : false case`) as syntactic sugar
+  def highest_bid
+    bids.last.bid
+  end
 
-	belongs_to :category
-	belongs_to :user
-	has_many :item_images, dependent: :destroy
-	accepts_nested_attributes_for :item_images #Nice!
-	has_many :bids
+  def highest_bidder
+    User.find(bids.last.user_id)
+  end
 
-	def highest_bid
-	  bids.last.bid
-	end
-
-	def highest_bidder
-	  User.find(bids.last.user_id)
-	end
-
-	def current_price
+  def current_price
     has_bids ? Bid.where(item_id: id).last.bid : starting_price
-	end
+  end
 
-	def has_bids
-	  bids.count > 0
-	end
+  def has_bids
+    bids.count > 0
+  end
 
-	def sold
+  def sold
     return false unless has_bids
-    bids.last.created_at < Date.today ? true : false
-	end
+    bids.last.created_at < Date.today
+  end
 
-	def imageDisplay #TskTsk, this method should be underscore_case, not camelCase
+  def imageDisplay
     picture || item_images[0].try(:image, :medium) || "missing.jpeg"
-	end
-
+  end
 end
