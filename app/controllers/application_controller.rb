0b2fca4a95097cf2b40bc94a5d58ff0a5f489b0e
Karen Kelley <karenkelley11@gmail.com>
2016-05-11 16:09:26 -0500

    Cleaning up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_devise_params, if: :devise_controller?
 
@@ -9,5 +7,4 @@ class ApplicationController < ActionController::Base
       u.permit(:email, :instagram_username, :password, :password_confirmation)
     end
   end
-
 end
