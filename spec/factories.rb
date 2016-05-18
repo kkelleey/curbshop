5e299c9f2930bef92427fc1c8d2258e69ac306d6
Karen Kelley <karenkelley11@gmail.com>
2016-05-18 12:14:50 -0500

    Refactoring user_deletes_item spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,10 +5,20 @@ FactoryGirl.define do
 
   factory :item do
     sequence(:description) { |n| "Description #{n}" }
+
+    factory :item_with_user do
+      user
+    end
   end
 
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
     password 'f4k3p455w0rd'
+
+    factory :user_with_item do
+      after(:create) do |user|
+        create(:item, user: user)
+      end
+    end
   end
 end
--- spec/features/user_deletes_items_spec.rb
+++ spec/features/user_deletes_items_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 RSpec.feature "UserDeletesItems", type: :feature do
   scenario 'item belongs to user' do
-    user = create :user
+    user = create :user_with_item
     login_as user
-    user.items << create(:item)
     visit curb_path
     click_on 'Remove from curb'
 
-    expect(page).to have_css(
-      '.alert', text: 'Success! Your item has been removed from your curb'
-    )
+    expect_page_to_have_alert('Success! Your item has been removed from your curb')
   end
 
   scenario 'item does not belong to user' do
     user = create :user
     login_as user
-    other_user = create :user
-    item = create(:item, user_id: other_user.id)
+    item = create :item_with_user
 
     page.driver.submit :delete, item_path(item.id), {}
 
-    expect(page).to have_css(
-      '.alert', text: 'You are not authorized to perform this action'
-    )
+    expect_page_to_have_alert('You are not authorized to perform this action')
     expect(current_path).to eq(root_path)
+  end
+
+  private
+
+  def expect_page_to_have_alert(text)
+    expect(page).to have_css('.alert', text: text)
   end
 end
