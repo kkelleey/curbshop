30bb4a4368458542b5b923a51286138382b91011
Karen Kelley <karenkelley11@gmail.com>
2016-05-18 15:34:28 -0500

    Moving unhappy delete to controller spec

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe ItemsController, type: :controller do
       end
     end
   end
+
+  describe 'DELETE #destroy' do
+    context 'item does not belong to user' do
+      it 'redirects to root path' do
+        user = create :user
+        sign_in user
+        item = create :item_with_user
+
+        delete :destroy, id: item.id
+
+        expect(response).to redirect_to root_path
+        expect(flash[:danger]).to match(
+          /^You are not authorized to perform this action$/
+        )
+      end
+    end
+  end
 end
--- spec/features/user_deletes_items_spec.rb
+++ spec/features/user_deletes_items_spec.rb
@@ -10,15 +10,4 @@ RSpec.feature "UserDeletesItems", type: :feature do
 
     expect(page).to have_alert('Success! Your item has been removed from your curb')
   end
-
-  scenario 'item does not belong to user' do
-    user = create :user
-    login_as user
-    item = create :item_with_user
-
-    page.driver.submit :delete, item_path(item.id), {}
-
-    expect(page).to have_alert('You are not authorized to perform this action')
-    expect(current_path).to eq(root_path)
-  end
 end
